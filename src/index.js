0a32839c39566ac7fd1299e175a4b25226e84cc7
Patrick Ford <patrickford@msn.com>
2018-07-16 00:44:48 -0700

    Basic client setup for simple auth

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import App from './components/app';
+import './components/index.css';
+import store from './store';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
